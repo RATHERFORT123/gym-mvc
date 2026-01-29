55cb18c7aef83f0c1b661d39099797347a8aa427
kali <kali@kali.kali>
2026-01-29 02:14:18 -0500

    Add feature 2

--- config/config.php
+++ config/config.php
@@ -1,12 +1,10 @@
 <?php
 define('BASE_URL', '/gym-mvc');
 
-
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'gym_db');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-
 
 define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_USER', 'rathorechetan114@gmail.com');
